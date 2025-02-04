8206f8b2e52579442965bb7c0ed2ecebc2231eb2
judekabeya <judek9392@gmail.com>
2025-02-04 14:14:26 +0100

    add files

---
+++ src/config.mjs
@@ -0,0 +1,12 @@
+export default {
+  development: {
+    type: 'development',
+    port: 3000,
+    mongodb: 'mongodb+srv://judekby:Jude93zoo@cluster0frei.zmsby.mongodb.net/ecole'
+  },
+  production: {
+    type: 'production',
+    port: 3000,
+    mongodb: 'mongodb+srv://judekby:Jude93zoo@cluster0frei.zmsby.mongodb.net/ecole'
+  }
+};
